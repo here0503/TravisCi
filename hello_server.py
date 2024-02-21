006b3e4e5c9d86b3852bdae13a446003dba5da05
here0503 <heresteban683@gmail.com>
2024-02-21 16:02:10 -0500

    thirt commit

--- hello_server.py
+++ hello_server.py
@@ -1,12 +1,3 @@
-import http.server
-import socketserver
+# hello_server.py
 
-# Configuración del puerto y dirección
-port = 8000
-handler = http.server.SimpleHTTPRequestHandler
-
-# Crear un servidor web
-with socketserver.TCPServer(("", port), handler) as httpd:
-    print(f"Servidor web en el puerto {port}")
-    # Iniciar el servidor
-    httpd.serve_forever()
+print("¡Hola, Mundo desde el servidor!")
